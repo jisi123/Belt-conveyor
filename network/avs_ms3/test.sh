399bbead4560fddebdf17b0e4dbc89aa40efd65a
jisi123 <15303717926@163.com>
2024-12-16 17:22:09 +0800

    Add files via upload

---
+++ network/avs_ms3/test.sh
@@ -0,0 +1,2 @@
+
+python test.py --session_name "MS3_resnet" --visual_backbone "resnet" --weights "MS3_resnet_best.pth"  --test_batch_size 1 --tpavi_stages 0 1 2 3 --tpavi_va_flag --save_pred_mask
---
+++ pretrained/readme.txt
@@ -0,0 +1,2 @@
+The pretrained visual/audio backbones are available at:
+https://pan.baidu.com/s/1XCxI3THok2Kdne-t3o5OnA?pwd=22qt
